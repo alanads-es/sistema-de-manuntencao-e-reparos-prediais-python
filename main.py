7ca4a54c790a1e88c67ef2a94d46825524ab0dff
glendhass <glendhass11@gmail.com>
2026-06-13 13:11:01 -0300

    Alterações no menu

--- main.py
+++ main.py
@@ -5,27 +5,21 @@ from options import addOrdemDeServico, viewOrdemDeServico, delOrdemDeServico
 valida a entrada do usuário e retorna uma opção válida entre 1 e 4
 além de que caso a entrada seja inválida, o menu é solicitado novamente'''
 def menu():
-    print("\n=========================================")
-    print("\nSistema de Manuntenção e Reparos Prediais\n")
-    print("=========================================\n")
-    print("[1] Adicionar ordem de serviço")
-    print("[2] Remover a ordem de serviço")
-    print("[3] Listar as ordens de serviço")
-    print("[4] Sair")
+    opcao = ""
+    while opcao not in ("1", "2", "3", "4"):
+        print("\n=========================================")
+        print("\nSistema de Manutenção e Reparos Prediais\n")
+        print("=========================================\n")
+        print("[1] Adicionar ordem de serviço")
+        print("[2] Remover a ordem de serviço")
+        print("[3] Listar as ordens de serviço")
+        print("[4] Sair")
+        opcao = input("Digite uma opção: ")
 
-    opcao = int(input("Digite uma opção: "))
+        if opcao not in ("1", "2", "3", "4"):
+            print("\nOpção indisponível, escolha uma opção entre 1 e 4!\n")
+    return opcao
 
-    '''Verifica se a entrada é numérica e dentro do intervalo permitido'''
-    if not opcao.isdigit():
-        print("\nOpção indisponível, escolha uma opção entre 1 e 4!\n")
-        return menu()
-
-
-    if opcao >= 1 and opcao <= 4:
-        return opcao
-    else:
-        print("\nOpção indisponível, escolha uma opção entre 1 e 5!\n")
-        return menu()
 
 '''Obtém a opção escolhida pelo usuário'''
 opcao = menu()
@@ -34,15 +28,16 @@ dataBase = loadData()
 
 '''Mantém o programa em execução até que o usuário escolha sair, 
 executando a funcionalidade correspondente à opção escolhida'''
-while opcao != 4:
-    if opcao == 1:
+while opcao != "4":
+    if opcao == "1":
         addOrdemDeServico(dataBase)
-    elif opcao == 2:
+    elif opcao == "2":
         delOrdemDeServico(dataBase)
-    elif opcao == 3:
+    elif opcao == "3":
         viewOrdemDeServico(dataBase)
 
-    '''Salva as alterações realizadas antes de encerrar o sistema'''
+    '''Salva as alterações realizadas no arquivo de dados a cada iteração'''
     saveData(dataBase)
     '''Solicita uma nova opção ao usuário'''
     opcao = menu()
+print("\nSistema encerrado!")
